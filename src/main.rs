4c7b274a3ce65bc874ec20b8f942fe89747605c2
TabulateJarl8 <tabulatejarl8@gmail.com>
2025-10-06 16:18:19 -0400

    get filename from CLI

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,12 @@ mod virtual_buffer;
 fn main() {
     env_logger::init();
 
+    let args: Vec<String> = std::env::args().collect();
+    let filename = args.get(1).expect("Expected path to a CHIP-8 program");
+    let data = std::fs::read(filename).expect("Expected a path to a CHIP-8 program");
+
     let mut emulator = Chip8::new();
+    emulator.load(&data);
 
     let mut window = Window::new(
         "CHIP-8 Emulator",
@@ -21,9 +26,6 @@ fn main() {
     .unwrap_or_else(|e| panic!("{}", e));
 
     window.set_target_fps(60);
-
-    let data = include_bytes!("1-chip8-logo.ch8");
-    emulator.load(data);
 
     while window.is_open() && !window.is_key_down(Key::Escape) {
         emulator.press_keys(&window.get_keys_pressed(KeyRepeat::No));
