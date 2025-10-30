b542dc9e1effc21c696286f998076033afe37de1
TabulateJarl8 <tabulatejarl8@gmail.com>
2025-10-29 22:44:58 -0400

    fix F v_reg shifting issue in 8xyE

--- src/emulator.rs
+++ src/emulator.rs
@@ -337,20 +337,19 @@ impl Chip8 {
             (8, reg_x, reg_y, 0xE) => {
                 log::trace!("SHL V{:X}", reg_x);
 
-                let shifted = if self.quirks.contains(Quirks::SHIFTING) {
+                if self.quirks.contains(Quirks::SHIFTING) {
+                    let lsb = (self.v_registers[reg_x as usize] >> 7) & 1;
                     // shifting quirk: only modifies vX
-                    let shifted_x = self.v_registers[reg_x as usize] << 1;
-                    self.v_registers[reg_x as usize] = shifted_x;
-                    shifted_x
+                    self.v_registers[reg_x as usize] <<= 1;
+                    // overflow register gets the most significant bit since it's the one chopped off
+                    self.v_registers[0xF] = lsb;
                 } else {
+                    let lsb = (self.v_registers[reg_y as usize] >> 7) & 1;
                     // normal behavior: sets vX to vY and then shifts vX
-                    let shifted_y = self.v_registers[reg_y as usize] << 1;
-                    self.v_registers[reg_x as usize] = shifted_y;
-                    shifted_y
-                };
-
-                // set overflow register to most significant bit
-                self.v_registers[0xF] = (shifted >> 7) & 1;
+                    self.v_registers[reg_x as usize] = self.v_registers[reg_y as usize] << 1;
+                    // overflow register gets the most significant bit since it's the one chopped off
+                    self.v_registers[0xF] = lsb;
+                }
             }
 
             (9, reg_x, reg_y, 0) => {
